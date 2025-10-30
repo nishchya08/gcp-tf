8ad756fab01c9d69ee87d0cef73b826efc9c653a
Nishchya Kataria <nishchyakataria@Nishchyas-MacBook-Air.local>
2025-10-30 23:58:54 +0530

    feat: add module version label in v1.0.2

--- modules/gcs_bucket/main.tf
+++ modules/gcs_bucket/main.tf
@@ -10,11 +10,13 @@ terraform {
 provider "google" {
   project = var.project_id
 }
-
 resource "google_storage_bucket" "this" {
   name                        = var.name
   location                    = var.location
   uniform_bucket_level_access = true
-  labels                      = var.labels
+  labels = merge(
+    var.labels,
+    { module_version = "v1.1.0" }  # <-- new behavior in v1.1.0
+  )
 }
 
