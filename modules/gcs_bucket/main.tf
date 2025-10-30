76ece1d1b1a08304a90d7d5dc4a97c49faf7e98a
Nishchya Kataria <nishchyakataria@Nishchyas-MacBook-Air.local>
2025-10-31 00:30:50 +0530

    feat: add module_version=v1.1.0 label

--- modules/gcs_bucket/main.tf
+++ modules/gcs_bucket/main.tf
@@ -8,11 +8,13 @@ terraform {
 }
 
 # No provider block in module
-
 resource "google_storage_bucket" "this" {
   name                        = var.name
   location                    = var.location
   uniform_bucket_level_access = true
-  labels                      = var.labels
+  labels = merge(
+    var.labels,
+    { module_version = "v1.1.0" }  # new behavior for v1.1.0
+  )
 }
 
